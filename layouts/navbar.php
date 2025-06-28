94acd6f1b291be61a4e581f0abf6757098412bb4
JhonnyHacker01 <2021210353@udh.edu.pe>
2025-06-27 23:26:43 -0500

    Mejorando

--- layouts/navbar.php
+++ layouts/navbar.php
@@ -1,7 +1,7 @@
 <nav>
     <ul>
-        <li><a href="registroUsuario.php">Registro</a></li>
-        <li><a href="login.php">Login</a></li>
-        <li><a href="dashboard.php">Dashboard</a></li>
+        <li><a href="//vistas/registroUsuario.php">Registro</a></li>
+        <li><a href="//vistas/login.php">Login</a></li>
+        <li><a href="//vistas/dashboard.php">Dashboard</a></li>
     </ul>
 </nav>
