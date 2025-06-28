4a58f0166ce4e3f1949e19c39b83170702a74cb4
Yhozira <2022110530@udh.edu.pe>
2025-06-27 23:54:02 -0500

    sdsd
    sdsd

--- vistas/login.php
+++ vistas/login.php
@@ -129,7 +129,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 <input type="password" name="contrasena" required>
             </label>
 
-            <button type="submit">Iniciar sesión</button>
+            <button type="submit">Iniciar sesion</button>
         </form>
 
         <p>¿No tienes cuenta? <a href="registroUsuario.php">Regístrate aquí</a></p>
